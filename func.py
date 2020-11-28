c0a0ec414467337968d9a3c88bd4d4ff74a3fb1d
chvanova.d <chvanovadasha@gmail.com>
2020-11-28 16:53:02 +0500

    202011281651 - парсинг с основной страницы - установка видимости элементов - печать частично закомментирована - try и таймер в parser.py временно закомментированы

--- func.py
+++ func.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.common.exceptions import TimeoutException
 import lot
+import time
 
 
 def toCutString(text, length=255):
@@ -17,9 +18,10 @@ def openAndParsePage(browser, link, listOfTenders):
     browser.get(link)
     tempForLinkText = link
     # getting number of pages
-    numberOfTenders = int(browser.find_element_by_xpath("//div[@class='filter__option filter--text pull-left']/div[1]/select/option").text.replace(' ','').replace('Активныеконкурсы:', ''))
-    numberOfPages = numberOfTenders // 15
-    if numberOfTenders % 15 > 0:
+    numberOfTenders = int(browser.find_element_by_xpath("//*[@id='results']/div/div[@class='row']/div[@class='col-md-6']/h4/strong/span[@class='badge badge-secondary']").text.replace(' ',''))
+    numberOfTendersOnPage = len(browser.find_elements_by_xpath("/html/body/main/div[3]/div/div[@class='col-lg-12']/div"))
+    numberOfPages = numberOfTenders // numberOfTendersOnPage
+    if numberOfTenders % numberOfTendersOnPage > 0:
         numberOfPages += 1
     # parsing from each page
     for i in range(numberOfPages):
@@ -33,60 +35,55 @@ def openAndParsePage(browser, link, listOfTenders):
                 print("\n===  NoSuchElementException  ===")
             finally:
                 continue
-    for tender in listOfTenders:
-        parseTenderLot(browser, tender)
-    # printLots(listOfTenders)
+    # for tender in listOfTenders:
+    #     parseTenderLot(browser, tender)
+    printLots(listOfTenders)
 
 
 def parseTendersFromPage(browser, listOfTenders, tempForLinkText):
-    # getting info from table
-    listForLotIDs = browser.find_elements_by_xpath("//div[@class='table-responsive']/table/tbody/tr/td[1]/a")
-    listForStartDates = browser.find_elements_by_xpath("//div[@class='table-responsive']/table/tbody/tr/td[2]")
-    listForPurchaseNames = browser.find_elements_by_xpath("//div[@class='table-responsive']/table/tbody/tr/td[3]")
-    listForCategories = browser.find_elements_by_xpath("//div[@class='table-responsive']/table/tbody/tr/td[4]")
-    listForEndDates = browser.find_elements_by_xpath("//div[@class='table-responsive']/table/tbody/tr/td[5]")
-    listForCustomerNames = browser.find_elements_by_xpath("//div[@class='table-responsive']/table/tbody/tr/td[6]")
-    listForCustomerCompanyNames = browser.find_elements_by_xpath("//div[@class='table-responsive']/table/tbody/tr/td[7]")
+    # making all elements visible
+    invisible_divs = browser.find_elements_by_xpath("//div[@class='strip_list wow fadeIn']")
+    for div in invisible_divs:
+        script = "arguments[0].style.visibility = 'visible';"
+        browser.execute_script(script, div)
 
-    for i in range(len(listForCustomerCompanyNames)):
-        listForLotIDs[i] = listForLotIDs[i].text
-        if listForCustomerNames[i] == "":
-            listForCustomerNames[i] = None
-        listForStartDates[i] = listForStartDates[i].text
-        listForEndDates[i] = listForEndDates[i].text
-        listForPurchaseNames[i] = listForPurchaseNames[i].text
-        if listForPurchaseNames[i] == "":
-            listForPurchaseNames[i] = None
-        listForCategories[i] = listForCategories[i].text
-        listForCustomerNames[i] = listForCustomerNames[i].text
-        if listForCustomerNames[i] == "":
-            listForCustomerNames[i] = None
-        listForCustomerCompanyNames[i] = listForCustomerCompanyNames[i].text
-        if listForCustomerCompanyNames[i] == "":
-            listForCustomerCompanyNames[i] = None
+    # getting info from page
+    list_for_lot_numbers = browser.find_elements_by_xpath("//div[@class='row']/div[@class='col-lg-6 strip_list_center']/ul/li[@class='strip_list_ow']/a")
+    list_for_started_at = browser.find_elements_by_xpath("//div[@class='row']/div[@class='col-lg-6 strip_list_center']/ul/li[contains(text(), 'Дата подачи') and @class='strip_list_ow']")
+    list_for_ended_at = browser.find_elements_by_xpath("//*[@id='my-soon-counter']")
+    list_for_names = browser.find_elements_by_xpath("//div[@class='col-lg-12']/div/p/a")
+    list_for_categories = browser.find_elements_by_xpath("//div[@class='col-lg-12']/div/small/a")
+
+    # formating text
+    for i in range(len(list_for_lot_numbers)):
+        list_for_lot_numbers[i] = list_for_lot_numbers[i].text.replace(' ', '').replace('ID:', '')
+        list_for_started_at[i] = list_for_started_at[i].text.replace('Дата подачи : ', '')
+        list_for_ended_at[i] = list_for_ended_at[i].get_attribute('data-due')
+        list_for_names[i] = list_for_names[i].text
+        if list_for_names[i] == "":
+            list_for_names[i] = None
+        list_for_categories[i] = list_for_categories[i].text.replace('#', '')
+        if list_for_categories[i] == "":
+            list_for_categories[i] = None
 
     # putting info in list
-    for i in range(len(listForLotIDs)):
+    for i in range(len(list_for_lot_numbers)):
         size = len(listOfTenders)
         listOfTenders.append(lot.Lot())
-        listOfTenders[size].lotID = int(listForLotIDs[i])
-        listOfTenders[size].linkToLot = (tempForLinkText + "/") + listForLotIDs[i]
-        listOfTenders[size].purchaseName = listForPurchaseNames[i]
-        listOfTenders[size].startDate = listForStartDates[i][0:10]
-        listOfTenders[size].endDate = listForEndDates[i][0:10] + " 23:59"
-        listOfTenders[size].category = listForCategories[i]
-        listOfTenders[size].customerName = listForCustomerNames[i]
-        listOfTenders[size].customerCompanyName = listForCustomerCompanyNames[i]
-        # parse current
+        listOfTenders[size].number = list_for_lot_numbers[i]
+        listOfTenders[size].source_url = (tempForLinkText + "/") + list_for_lot_numbers[i]
+        listOfTenders[size].name = list_for_names[i]
+        listOfTenders[size].started_at = list_for_started_at[i][0:19]
+        listOfTenders[size].ended_at = list_for_ended_at[i][0:19]
+        listOfTenders[size].category = list_for_categories[i]
+        listOfTenders[size].type = 'tender'
 
     # clear lists
-    listForLotIDs.clear()
-    listForStartDates.clear()
-    listForPurchaseNames.clear()
-    listForCategories.clear()
-    listForEndDates.clear()
-    listForCustomerNames.clear()
-    listForCustomerCompanyNames.clear()
+    list_for_lot_numbers.clear()
+    list_for_started_at.clear()
+    list_for_names.clear()
+    list_for_categories.clear()
+    list_for_ended_at.clear()
 
 
 def parseTenderLot(browser, currentTender):
@@ -136,7 +133,7 @@ def parseTenderLot(browser, currentTender):
     except NoSuchElementException:
         currentTender.specialConditions = None
 
-    currentTender.type = "Тендер"
+    currentTender.type = "tender"
 
     content = browser.page_source
     if "Вложение" in content:
@@ -145,26 +142,26 @@ def parseTenderLot(browser, currentTender):
         currentTender.attachedFile = None
 
 
-# def printLots(listOfTenders):
-#     tempCountForPrint = 1
-#     for tender in listOfTenders:
-#         print("#", tempCountForPrint,
-#               "\n  lotID", tender.lotID,
-#               "\n  type\n   ", tender.type,
-#               "\n  category\n   ", tender.category,
-#               "\n  linkToLot\n   ", tender.linkToLot,
-#               "\n  startDate\n   ", tender.startDate,
-#               "\n  endDate\n   ", tender.endDate,
-#               "\n  status\n   ", tender.status,
-#               "\n  customerAddressArea\n   ", tender.customerAddressArea,
-#               "\n  purchaseName\n   ", tender.purchaseName,
-#               "\n  customerName\n   ", tender.customerName,
-#               "\n  attachedFile\n   ", tender.attachedFile,
-#               "\n  paymentTerm\n   ", tender.paymentTerm,
-#               "\n  customerCompanyName\n   ", tender.customerCompanyName,
-#               "\n  customerPhone\n   ", tender.customerPhone,
-#               "\n  customerEmail\n   ", tender.customerEmail,
-#               "\n  specialConditions\n   ", tender.specialConditions,
-#               "\n  deliveryTerm\n   ", tender.deliveryTerm,
-#               "\n ============================\n")
-#         tempCountForPrint += 1
+def printLots(listOfTenders):
+    tempCountForPrint = 1
+    for tender in listOfTenders:
+        print("#", tempCountForPrint,
+              "\n  number\n   ", tender.number,
+              "\n  type\n   ", tender.type,
+              "\n  category\n   ", tender.category,
+              "\n  source_url\n   ", tender.source_url,
+              "\n  started_at\n   ", tender.started_at,
+              "\n  ended_at\n   ", tender.ended_at,
+              # "\n  status\n   ", tender.status,
+              # "\n  customerAddressArea\n   ", tender.customerAddressArea,
+              "\n  name\n   ", tender.name,
+              # "\n  customerName\n   ", tender.customerName,
+              # "\n  attachedFile\n   ", tender.attachedFile,
+              # "\n  paymentTerm\n   ", tender.paymentTerm,
+              # "\n  customerCompanyName\n   ", tender.customerCompanyName,
+              # "\n  customerPhone\n   ", tender.customerPhone,
+              # "\n  customerEmail\n   ", tender.customerEmail,
+              # "\n  specialConditions\n   ", tender.specialConditions,
+              # "\n  deliveryTerm\n   ", tender.deliveryTerm,
+              "\n ============================\n")
+        tempCountForPrint += 1
